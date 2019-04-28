b27f856dececba3d6a774187960e01c10230502c
Shuhei Nishizawa <shuhey.dev@gmail.com>
2019-04-28 13:44:08 +0900

    プロジェクト ファイルを追加します。

---
+++ MarkdigSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Markdig;
+
+namespace MarkdigTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //マークダウンファイルの読み込み
+            var md = File.ReadAllText(@"C:\Users\shuhey\Desktop\markdigReadMe.md");
+
+            //HTMLへの変換
+            var html = Markdown.ToHtml(md);
+
+            //HTMLファイルの出力
+            File.WriteAllText(@"C:\Users\shuhey\Desktop\markdigReadMe.html", html);
+
+            Console.WriteLine(html);
+
+        }
+    }
+}
